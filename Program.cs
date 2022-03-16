4c3018b3e1bdb94e96268ccd3f6fe8592a34e6ef
murgendrag <murgendra67@gmail.com>
2022-03-16 22:11:43 +0530

    Call in main

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linkedlist
+{
+    class Program
+    {
+        static void  Main (string[] args)
+        {
+            Linkedlist1 linkedlist = new Linkedlist1();
+
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(70);
+            linkedlist.Display();
+           
+        }
+    }
+}
+
